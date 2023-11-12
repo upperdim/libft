2e6a06874cb14f249d4b17eb4269af6f4cc321e7
upperdim <45048352+upperdim@users.noreply.github.com>
2023-11-12 19:54:00 +0100

    removed dependent functions

--- ft_calloc.c
+++ ft_calloc.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   ft_calloc.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: tunsal <tunsal@student.42heilbronn.de>     +#+  +:+       +#+        */
+/*   By: tunsal <tunsal@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/06 17:36:46 by tunsal            #+#    #+#             */
-/*   Updated: 2023/10/20 14:59:03 by tunsal           ###   ########.fr       */
+/*   Updated: 2023/11/12 19:51:13 by tunsal           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,6 +21,7 @@ void	*ft_calloc(size_t elems_count, size_t elem_size)
 {
 	size_t	alloc_size;
 	void	*ptr;
+	size_t	i;
 
 	alloc_size = elems_count * elem_size;
 	if (elem_size != 0 && alloc_size / elem_size != elems_count)
@@ -28,6 +29,11 @@ void	*ft_calloc(size_t elems_count, size_t elem_size)
 	ptr = malloc(alloc_size);
 	if (ptr == NULL)
 		return (NULL);
-	ft_bzero(ptr, alloc_size);
+	i = 0;
+	while (i < alloc_size)
+	{
+		*((unsigned char *) ptr + i) = (unsigned char) 0;
+		++i;
+	}
 	return (ptr);
 }
